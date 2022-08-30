e3772569ed28a46ff1dff983065a8bec01cb4542
Danixd12 <droidgamersyt@gmail.com>
2022-08-30 11:07:15 +0200

    1.2

--- src/main/java/d/studio/messageapi/MessageAPI.kt
+++ src/main/java/d/studio/messageapi/MessageAPI.kt
@@ -3,10 +3,13 @@ package d.studio.messageapi
 import d.studio.messageapi.api.Msg
 import org.bukkit.plugin.java.JavaPlugin
 
+
 class MessageAPI : JavaPlugin() {
 
     lateinit var api: Msg
+    lateinit var Instance: MessageAPI
     override fun onEnable() {
+        Instance = this
         api = Msg()
     }
 
